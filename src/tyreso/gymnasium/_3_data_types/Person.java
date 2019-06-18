8744e8350097de0b2cef57ed9f0aa07c82356dd8
Carl-Granstrom <35761913+Carl-Granstrom@users.noreply.github.com>
2019-06-18 23:17:42 +0200

    Person variables data types for discussion.

---
+++ src/tyreso/gymnasium/_3_data_types/Person.java
@@ -0,0 +1,16 @@
+package tyreso.gymnasium._3_data_types;
+
+public class Person {
+
+    int age;
+    String name;
+    String adress;
+    double heightInCentimeters;
+    int heightInMillimeters;
+    // TODO discuss! weight;
+    char englishGrade;
+    char mathGrade;
+    char swedishGrade;
+    // TODO discuss!  phoneNumber;
+    // TODO discuss!  personalIdentificationNumber;
+}
