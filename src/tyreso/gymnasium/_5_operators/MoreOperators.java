03ff8ae3c72859f1f51210ee7b819a80a32994b5
Carl Granstrom <carl.grnstrm@gmail.com>
2019-06-26 03:08:05 +0200

    Added _5_operators example code and exercise code.

---
+++ src/tyreso/gymnasium/_5_operators/MoreOperators.java
@@ -0,0 +1,104 @@
+package tyreso.gymnasium._5_operators;
+
+/**
+ * Example code for the course Programmering 1. The purpose of the code is showing some of the operators in use.
+ *
+ * The code is also a good example of purely sequential code as it has no selection and no iteration.
+ *
+ * NEVER write your actual code like this.
+ *
+ * @author Carl Granström
+ */
+public class MoreOperators {
+
+    public static void main(String[] args) {
+
+        //Variabedeklaration och initialisation
+        long numOne = 3;
+        System.out.println("numOne = " + numOne);
+        long numTwo = 4;
+        System.out.println("numTwo = " + numTwo);
+        long numThree = 4;
+        System.out.println("numThree = " + numThree);
+        long numFour = 9;
+        System.out.println("numFour = " + numFour);
+        System.out.println();
+
+
+        // ++ operatören(increment)
+        numOne++;   //increment postfix
+        ++numOne;   //increment prefix
+        System.out.println("Resultatet av två increment av numOne(3) är: " + numOne);
+        System.out.println("Resultatet borde vara 5");
+        System.out.println();
+
+        // -- operatören(decrement)
+        numOne--;   //decrement postfix
+        --numOne;   //decrement prefix
+        numOne--;
+        numOne--;
+        numOne--;
+        numOne--;
+        System.out.println("Resultatet av sex decrement av numOne är: " + numOne);
+        System.out.println("Resultatet borde vara -1");
+        System.out.println();
+
+        // % operatören(modulo/modus/remainder)
+        long moduloResult = numFour % 3;
+        System.out.println("Resultatet av numFour(9) modulo 3 är: " + moduloResult);
+        System.out.println("Resultatet borde vara 0");
+        moduloResult = numFour % 5;
+        System.out.println("Resultatet av numFour(9) modulo 5 är: " + moduloResult);
+        System.out.println("Resultatet borde vara 4");
+        System.out.println();
+
+        // <= operatören(smaller or equals)
+        //TODO CODE TOGETHER
+        System.out.println("<= operatören(smaller or equals): ");
+        System.out.println( 4 <= 3 );   //TODO: true/false?
+        System.out.println( 4 <= 4 );   //TODO: true/false?
+        System.out.println( 4 <= 5 );   //TODO: true/false?
+        System.out.println("Resultatet borde vara: ?, ?, ?, ?");
+        System.out.println();
+
+        // >= operatören(greater or equals)
+        //TODO CODE TOGETHER
+        System.out.println(">= operatören(greater or equals): ");
+        System.out.println( 4 >= 3 );   //TODO: true/false?
+        System.out.println( 4 >= 4 );   //TODO: true/false?
+        System.out.println( 4 >= 5 );   //TODO: true/false?
+        System.out.println("Resultatet borde vara: ?, ?, ?, ?");
+        System.out.println();
+
+        // && operatören(logisk OCH /logical AND operatör)
+        System.out.println("&& operatören: ");
+        System.out.println( true && true );     //TODO: true/false?
+        System.out.println( true && false );    //TODO: true/false?
+        System.out.println( false && true );    //TODO: true/false?
+        System.out.println( false && false );   //TODO: true/false?
+        System.out.println("Resultatet borde vara: ?, ?, ?, ?");
+        System.out.println();
+
+        //  || operatören(logisk ELLER / OR operatör)
+        System.out.println("|| operatören ");
+        System.out.println( true || true );     //TODO: true/false?
+        System.out.println( true || false );    //TODO: true/false?
+        System.out.println( false || true );    //TODO: true/false?
+        System.out.println( false || false );   //TODO: true/false?
+        System.out.println("Resultatet borde vara: ?, ?, ?, ?");
+        System.out.println();
+
+        //mer variabeldeklaration och initialisation
+        int a = 2;
+
+        //sammansatta operatörer
+        a += 3; //a = 5
+        //TODO Vad ska vi skriva för att kolla om a är 5?
+        a -= 7; //a = -2
+        //TODO ...och här?
+        a *= 3; //a = TODO?
+        //TODO ...och här?
+        a /= -3;
+        //TODO ...och vad blev nu a till slut?
+    }
+}
---
+++ src/tyreso/gymnasium/_5_operators/_5_operators_exercises/OddOrEven.java
@@ -0,0 +1,45 @@
+package tyreso.gymnasium._5_operators._5_operators_exercises;
+
+/**
+ * Programming riddle, use the operators to figure some things out.
+ */
+public class OddOrEven {
+
+    public static void main(String[] args) {
+        long a = 912642198;
+        long b = 1985612321;
+        long c = 73;
+
+        /**
+         * 1. Kontrollera vilken av variablerna a, b och c är jämn och vilka som är udda med hjälp av en av operatörerna
+         * 2. Skriv ut ditt resultat på ett användarvänligt(läsbart) sätt till konsolen
+         * HINT: Lösningen bör ge svaret 0 för jämna nummer och svaret ett för ojämna
+         *
+         * 3. Kontrollera att resten av 9 / 5 är större än eller lika med fyra
+         * 3.1 Skriv ut resultatet på ett tydligt sätt
+         *
+         * 4.1 Börja med siffran 3 i en variabel
+         * 4.2 Öka med ett
+         * 4.3 Dra ifrån ett, två gånger på raken
+         * 4.4 Ta svaret gånger 3
+         * 4.5 Kontrollera om svaret är lika med 6 eller 8 i två separata anrop och skriv ut om din kontroll returnerar
+         *      sant eller falskt(boolean)
+         */
+
+        //TODO YOUR CODE HERE!!!   YOUR CODE HERE!!!   YOUR CODE HERE!!!   YOUR CODE HERE!!!   YOUR CODE HERE!!!
+
+
+        //TODO BONUS CHALLENGE BELOW!!!  BONUS CHALLENGE BELOW!!!  BONUS CHALLENGE BELOW!!!  BONUS CHALLENGE BELOW!!!
+        /**
+         * Använd operatörerna tillsammans med if-satser (Lektion 7) för att skriva ut olika strängar beroende på om
+         * vissa siffror är större/mindre/lika med andra siffror eller om vissa uttryck är sanna eller falska rent
+         * generellt. Ju fler operatörer du använder desto bättre. Testa att kombinera olika operatörer.
+         *
+         * Exempel nedan.
+         */
+        int numOne = 3;
+        if (((numOne == 3) && (numOne > 0)) || (5 > 5)) {
+            System.out.println("Well that's weird");
+        }
+    }
+}
