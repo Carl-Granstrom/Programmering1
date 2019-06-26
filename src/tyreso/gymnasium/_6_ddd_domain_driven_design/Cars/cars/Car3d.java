8264e5784064057fbecb95604cf11a88d9830d47
Carl Granstrom <carl.grnstrm@gmail.com>
2019-06-26 14:49:43 +0200

    Added _6_ddd_domain_driven_design example code.

---
+++ src/tyreso/gymnasium/_6_ddd_domain_driven_design/Cars/cars/Car3d.java
@@ -0,0 +1,7 @@
+package tyreso.gymnasium._6_ddd_domain_driven_design.Cars.cars;
+
+/**
+ * Add variables and methods to make this version of the Car-class something that functions for a 3d driving game.
+ */
+public class Car3d {
+}
---
+++ src/tyreso/gymnasium/_6_ddd_domain_driven_design/Cars/cars/Card.java
@@ -0,0 +1,19 @@
+package tyreso.gymnasium._6_ddd_domain_driven_design.Cars.cars;
+
+/**
+ * This class represents the cards in a deck of cards.
+ *
+ * @param ??? represents the suite of the card as ???
+ * @param ??? represents the value of the card as ???
+ * TODO DISCUSS: How should we handle the fact that card values have specific names: Ace, Knight/Cavalier, Queen, King?
+ * TODO DISCUSS: What else do we need when it comes to the STATE and the BEHAVIOUR of a card?
+ */
+public class Card {
+
+    //Declare variables
+
+    //Constructor
+
+    //Methods
+
+}
