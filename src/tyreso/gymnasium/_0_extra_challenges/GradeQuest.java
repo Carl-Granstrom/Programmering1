1d62e33c53a60f4694cef3066b15ba51b27e3865
Carl Granstrom <carl.grnstrm@gmail.com>
2019-08-22 23:12:01 +0200

    added _0_extra_challenges added GradeQuest to _0_extra_challenges

---
+++ src/tyreso/gymnasium/_0_extra_challenges/GradeQuest.java
@@ -0,0 +1,37 @@
+package tyreso.gymnasium._0_extra_challenges;
+
+/**
+ *                          *******************
+ *                          *** GRADE QUEST ***
+ *                          *******************
+ *
+ * Den här utmaningen är lämplig att försöka sig på efter lektion 10 eller 11.
+ *
+ * Instruktionen lyder som följer:
+ *
+ * Skapa ett program som lagrar alla namn i klassen samt alla de begrepp som finns i kursplanen plus några som tagits
+ * upp på lektionerna. När programmet körs ska det slumpmässigt välja ett namn på en elev som ska svara och ett begrepp
+ * vilket eleven ska förklara betydelsen av(inte i programmet dock).
+ *
+ * Eftersom ni inte gärna själva vill behöva svara på någon svår fråga så ser ni så klart till att ert eget namn aldrig
+ * dras. MEN, det finns en hake: ni MÅSTE ha med ert eget namn i listan över namn. Det finns flera sätt att lösa det
+ * problemet, men ju subtilare lösning desto bättre: med andra ord, ju svårare det är att hitta varför ert namn inte
+ * dras desto mer pluspoäng.
+ *
+ * Dessutom ska GradeQuest både kunna köras direkt för att producera output OCH kunna anropas utan att instantieras.
+ * Programmet ska i det senare fallet returnera en String/sträng med elevnamnet och begreppet.
+ *
+ * Bonus 1: Bygg också en command-line interface till programmet som tillåter användaren att dra flera
+ * namn/begreppskombinationer i rad.
+ *
+ * Bonus 2: Se också till att samma begrepp inte kan slumpas fram igen innan alla begrepp gåtts igenom. Se till att
+ * programmet meddelar användaren när alla svarat en gång.
+ *
+ * Bonus 3: Låt användaren sjäv bestämma om samma begrepp ska kunna återkomma eller inte. Låt användaren själv bestämma
+ * om alla elever ska ha svarat innan en elev kan få frågan igen eller om en elev ska kunna få frågan flera gånger i
+ * rad.
+ *
+ *
+ */
+public class GradeQuest {
+}
