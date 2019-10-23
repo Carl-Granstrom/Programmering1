9152e5287bdfcdfb4f15222b5e6d3ee118027cfd
Carl-Granstrom <carl.grnstrm@gmail.com>
2019-10-23 14:37:06 +0200

    Created _11_collections

--- src/tyreso/gymnasium/cardgame/Hand.java
+++ src/tyreso/gymnasium/cardgame/Hand.java
@@ -1,4 +1,39 @@
 package tyreso.gymnasium.cardgame;
 
+import java.util.ArrayList;
+
 public class Hand {
+    int currentNumCards;
+    int maxCards;
+    ArrayList<Card> cards;
+
+    Hand(){
+        this.currentNumCards = 0;
+        this.maxCards = 10000;
+        this.cards = new ArrayList<Card>();
+    }
+
+    Hand(int maxCards){
+        this.currentNumCards = 0;
+        this.maxCards = maxCards;
+        this.cards = new ArrayList<Card>();
+    }
+
+    Hand(String gameName){
+        this.currentNumCards = 0;
+        this.cards = new ArrayList<Card>();
+        if (gameName.equals("GoFish")) {
+            this.maxCards = 999;
+        } else if (gameName.equals("BlackJack")){
+            this.maxCards = 2;
+        } else {
+            this.maxCards = 10000;
+        }
+    }
+
+    public void drawCard(Card card){
+        cards.add(card);
+    }
+
+
 }
